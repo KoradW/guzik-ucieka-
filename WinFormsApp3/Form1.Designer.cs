63bf5feb1a486510f6e78e5dd7c63e20b005320b
2142615 <2142615@TE-GDA-D-D21E10>
2024-10-11 16:03:17 +0200

    Dodaj pliki projektów.

---
+++ WinFormsApp3/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WinFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            button1 = new Button();
+            timer1 = new System.Windows.Forms.Timer(components);
+            statusStrip1 = new StatusStrip();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(306, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 77);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += timer1_Tick;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 1;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private StatusStrip statusStrip1;
+    }
+}
---
+++ WinFormsApp3/Form1.cs
@@ -0,0 +1,40 @@
+namespace WinFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        readonly Random r;
+        int punkty;
+        public Form1()
+        {
+            InitializeComponent();
+            r= new Random();
+            punkty = 0;
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveButton();
+            punkty = 0;
+            statusStrip1.Text = "Punkty: " + punkty.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MoveButton();
+            punkty++;
+            statusStrip1.Text = "Punkty: " + punkty.ToString();
+            timer1.Stop();
+            timer1.Start();
+        }
+
+        private void MoveButton()
+        {
+            int maxX = this.Size.Width - button1.Size.Width - 50;
+            int maxY = this.Size.Height - button1.Size.Height - 50;
+            Point p = new Point();
+            p.X = r.Next(12, maxX);
+            p.Y = r.Next(12, maxY);
+            button1.Location = p;
+        }
+    }
+}
